96f1835ab2d81ab68b25f24f101edb8cfd85c526
Nurs <nursultanerbakytuly88@gmail.com>
2025-04-04 04:08:52 +0500

    adding new function find all and find one or none

--- repository/base.py
+++ repository/base.py
@@ -4,9 +4,22 @@ from sqlalchemy import select
 
 class BaseRepository:
     model = None
+
+
+
+
     @classmethod
-    async def find_all(cls):
+    async def find_one_or_none(cls, **filter_by):
         async with async_session_maker() as session:
-            query = select(cls.model)
+            query = select(cls.model).filter_by(**filter_by)
+            result= await session.execute(query)
+            return result.scalars_one_or_none()
+
+
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.scalars().all()
