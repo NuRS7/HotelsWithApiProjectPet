9883577d16478a0e601609e966c1066b812b60e0
Nurs <nursultanerbakytuly88@gmail.com>
2025-04-04 00:44:52 +0500

    Валидация через настройки  декоратор modal_validator и импортируем скрывая главные настройки нашего проекта из .env

--- config.py
+++ config.py
@@ -1,17 +1,27 @@
 from pydantic_settings import BaseSettings
+from pydantic import model_validator
 
 class Settings(BaseSettings):
-    DB_HOST:str
-    DB_PORT:int
-    DB_USER:str
-    DB_PASS:str
-    DB_NAME:str
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    DATABASE_URL: str = ""
 
-# Осы жерге .env данныйларды коямыз
+    @model_validator(mode="after")
+    def set_database_url(cls, values):
+        values.DATABASE_URL = f"postgresql+asyncpg://{values.DB_USER}:{values.DB_PASS}@{values.DB_HOST}:{values.DB_PORT}/{values.DB_NAME}"
+        return values
+    # Осы жерге .env данныйларды коямыз
     class Config:
-        env_file = '.env'
+        env_file = ".env"
+
 settings = Settings()
 
-print(settings.DB_HOST)
 
+
+
+# DATABASE_URL=f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(settings.DATABASE_URL)
 #python.exe .\config.py чекинг
