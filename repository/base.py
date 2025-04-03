c41ad3503082a510b39518f8be9b17be4c86c32c
Nurs <nursultanerbakytuly88@gmail.com>
2025-04-04 03:56:37 +0500

    Added new features BaseRepository чтобы сократить код наследуемся от него

---
+++ repository/base.py
@@ -0,0 +1,12 @@
+from database import async_session_maker
+from sqlalchemy import select
+
+
+class BaseRepository:
+    model = None
+    @classmethod
+    async def find_all(cls):
+        async with async_session_maker() as session:
+            query = select(cls.model)
+            result = await session.execute(query)
+            return result.scalars().all()
