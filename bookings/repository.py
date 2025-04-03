c1814b27999b8d7ed33c19fb7e9f4e2e56de4092
Nurs <nursultanerbakytuly88@gmail.com>
2025-04-04 03:58:40 +0500

    fixing bags

--- bookings/repository.py
+++ bookings/repository.py
@@ -1,18 +1,12 @@
-from database import async_session_maker
-from sqlalchemy import select
+
 from bookings.models import Bookings
+from repository.base import BaseRepository
 
 
 
 
-class BookingRepository:
-    @classmethod
-    async def find_all(cls):
-        async with async_session_maker() as session:
-            query = select(Bookings)
-            bookings = await session.execute(query)
-            return bookings.scalars().all()
-
+class BookingRepository(BaseRepository):
+    model = Bookings
 
 
 
