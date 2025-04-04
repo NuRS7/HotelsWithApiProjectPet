100767837fee1524f353c7809f53d6631134b2ba
Nurs <nursultanerbakytuly88@gmail.com>
2025-04-04 05:34:18 +0500

    SECRET_KEY: str ALGORITHM: str

--- config.py
+++ config.py
@@ -13,6 +13,11 @@ class Settings(BaseSettings):
     def set_database_url(cls, values):
         values.DATABASE_URL = f"postgresql+asyncpg://{values.DB_USER}:{values.DB_PASS}@{values.DB_HOST}:{values.DB_PORT}/{values.DB_NAME}"
         return values
+
+
+    SECRET_KEY: str
+    ALGORITHM: str
+
     # Осы жерге .env данныйларды коямыз
     class Config:
         env_file = ".env"
