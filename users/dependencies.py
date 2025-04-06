cb1e89afb811144a339c58e05879a473e2f14623
Nurs <nursultanerbakytuly88@gmail.com>
2025-04-06 13:28:21 +0500

    added exceptions from exceptions.py to dependencies.py

--- users/dependencies.py
+++ users/dependencies.py
@@ -1,38 +1,36 @@
-from datetime import datetime, timezone
-from fastapi import Request, HTTPException, status, Depends
-from jose import jwt, JWTError
-from users.models import Users
+from fastapi import Depends, Request
+from jose import ExpiredSignatureError, JWTError, jwt
+
 from config import settings
+from exceptions import (
+    IncorrectTokenFormatException,
+    TokenAbsentException,
+    TokenExpiredException,
+    UserIsNotPresentException,
+)
 from users.repository import UserRepository
 
 
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY)
+        raise TokenAbsentException
     return token
 
 
-async def get_current_user(token:str =Depends(get_token)): #get token функцияга зависит
+async def get_current_user(token: str = Depends(get_token)):
     try:
-        payload = jwt.decode(
-            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
-        )
+        payload = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+    except ExpiredSignatureError:
+
+        raise TokenExpiredException
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    expire: str = payload.get("exp")
-    if not expire or datetime.fromtimestamp(int(expire), tz=timezone.utc) < datetime.now(timezone.utc):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    user_id : str = payload.get("sub")
+        raise IncorrectTokenFormatException
+    user_id: str = payload.get("sub")
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    user = await UserRepository.find_one_or_none(id =int(user_id))
+        raise UserIsNotPresentException
+    user = await UserRepository.find_one_or_none(id=int(user_id))
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        raise UserIsNotPresentException
+
     return user
-
-
-async def get_current_admin_user(current_user: Users = Depends(get_current_user)):
-    # if current_user.role !="admin":
-    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    return current_user
