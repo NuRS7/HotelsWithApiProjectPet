d49df052858496759cd757f4ba79199cc8368ffd
Nurs <nursultanerbakytuly88@gmail.com>
2025-04-06 13:16:55 +0500

    Добавил изменения к dao

--- bookings/repository.py
+++ bookings/repository.py
@@ -1,14 +1,107 @@
-
+from sqlalchemy.exc import SQLAlchemyError
 from bookings.models import Bookings
 from repository.base import BaseRepository
-
-
-
+from datetime import date
+from sqlalchemy import delete, or_, select, insert, func, and_
+from database import async_session_maker
+from hotels.rooms.models import Rooms
+from exceptions import RoomFullyBooked
 
 class BookingRepository(BaseRepository):
     model = Bookings
 
 
+    @classmethod
+    async def add(cls,
+                  user_id:int,
+                  room_id:int,
+                  date_from:date,
+                  date_to:date,
+                  ):
+        try:
+            async with async_session_maker() as session:
+                booked_rooms = (
+                    select(Bookings)
+                    .where(
+                        and_(
+                            Bookings.room_id == room_id,
+                            or_(
+                                and_(
+                                    Bookings.date_from >= date_from,
+                                    Bookings.date_from <= date_to,
+                                ),
+                                and_(
+                                    Bookings.date_from <= date_from,
+                                    Bookings.date_to > date_from,
+                                ),
+                            ),
+                        )
+                    )
+                    .cte("booked_rooms")
+                )
+
+
+                get_rooms_left = (
+                    select(
+                        (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
+                            "rooms_left"
+                        )
+                    )
+                    .select_from(Rooms)
+                    .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
+                    .where(Rooms.id == room_id)
+                    .group_by(Rooms.quantity, booked_rooms.c.room_id)
+                )
+
+                # Рекомендую выводить SQL запрос в консоль для сверки
+                # logger.debug(get_rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
+
+                rooms_left = await session.execute(get_rooms_left)
+                rooms_left: int = rooms_left.scalar()
+
+
+
+                if rooms_left > 0:
+                    get_price = select(Rooms.price).filter_by(id=room_id)
+                    price = await session.execute(get_price)
+                    price: int = price.scalar()
+                    add_booking = (
+                        insert(Bookings)
+                        .values(
+                            room_id=room_id,
+                            user_id=user_id,
+                            date_from=date_from,
+                            date_to=date_to,
+                            price=price,
+                        )
+                        .returning(
+                            Bookings.id,
+                            Bookings.user_id,
+                            Bookings.room_id,
+                            Bookings.date_from,
+                            Bookings.date_to,
+                        )
+                    )
+
+                    new_booking = await session.execute(add_booking)
+                    await session.commit()
+                    return new_booking.mappings().one()
+                else:
+                    raise RoomFullyBooked
+        except RoomFullyBooked:
+            raise RoomFullyBooked
+        except (SQLAlchemyError, Exception) as e:
+            if isinstance(e, SQLAlchemyError):
+                msg = "Database Exc: Cannot add booking"
+            elif isinstance(e, Exception):
+                msg = "Unknown Exc: Cannot add booking"
+            extra = {
+                "user_id": user_id,
+                "room_id": room_id,
+                "date_from": date_from,
+                "date_to": date_to,
+            }
+            # logger.error(msg, extra=extra, exc_info=True)
 
 
 
